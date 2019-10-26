8bb546e060aded9a874dcec45944a18058ea330a
mpotoczny <wiatrak20>
2019-10-26 20:41:54 +0200

    Test

---
+++ zad_003_zagadka_mat.py
@@ -0,0 +1,7 @@
+### Zadanie 2.1 | Zagadka matematyczna
+#
+#Program losuje dwie liczby z zakresu od 0 do 99 (patrz poniżej). Podaje te dwie liczby
+# i pyta jaka jest ich suma (nie podaje jej). Użytkownik ma odgadnąć (no, policzyć w głowie) wynik.
+# Program pyta o wynik wielokrotnie, tak długo, aż użytkownik poda prawidłowy wynik.
+#
+
