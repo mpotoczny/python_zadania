f34172f9813245f79989e04d5bac94a07c6e1dba
mpotoczny <wiatrak20>
2020-02-18 14:24:07 +0100

    po kursie 1 z pracy

--- klasy/powtorka/domowe/z1_ogloszenia.py
+++ klasy/powtorka/domowe/z1_ogloszenia.py
@@ -1,4 +1,29 @@
 # Zadanie 4.1 | Ogłoszenia Zaproponuj klasę, w której obiektach
 # będzie się zapisywać ogłoszenia (takie jak w serwisie internetowym z ogłoszeniami).
 
-class Ogloszenia:
+class Ogloszenie:
+    _next_id = 1 #atrybut klasowy
+
+    def __init__(self, tytul, opis, cena, telefon, miejscowosc):
+        self.id = Ogloszenie._next_id
+        Ogloszenie._next_id += 1
+
+        self.tytul = tytul
+        self.opis = opis
+        self.cena = cena
+        self.telefon = telefon
+        self.miejscowosc = miejscowosc
+
+    def __str__(self):
+        tresc = f'\nID: {self.id}\n' \
+                f'{self.cena} PLN, {self.miejscowosc.upper()}, tel.: {self.telefon}, ' \
+                f'{self.tytul.upper()}\n' \
+                f'{self.opis}\n***'
+        return tresc
+
+
+ogl1 = Ogloszenie('pomoc domowa', 'Pomogę w domu przy zwierzętach i ludziach', 80, 123456, 'WrocłAW')
+ogl2 = Ogloszenie('Katering na urodziny', 'Zrobie placków', 120, 22123456, 'kenia północna')
+
+print(ogl1)
+print(ogl2)
